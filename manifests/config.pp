f52daa8a96ec301b0ec7cec101295ea8a63605ce
kreczko <lkreczko@gmail.com>
2013-06-23 19:57:01 +0100

    adding configuration files

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,36 @@
-class apelpublisher::config {
-
+class apelpublisher::config (
+  $mysql_hostname          = $apelpublisher::params::mysql_hostname,
+  $mysql_port              = $apelpublisher::params::mysql_port,
+  $mysql_database          = $apelpublisher::params::mysql_database,
+  $mysql_user              = $apelpublisher::params::mysql_user,
+  $mysql_password          = $apelpublisher::params::mysql_apel_password,
+  $site_name               = $apelpublisher::params::site_name,
+  $ldap_host               = $apelpublisher::params::ldap_host,
+  $ldap_port               = $apelpublisher::params::ldap_port,
+  $joiner_local_jobs       = $apelpublisher::params::joiner_local_jobs,
+  $joiner_enabled          = $apelpublisher::params::joiner_enabled,
+  $unloader_enabled        = $apelpublisher::params::unloader_enabled,
+  $unloader_dir_location   = $apelpublisher::params::unloader_dir_location,
+  $unloader_send_summaries = $apelpublisher::params::unloader_send_summaries,
+  $unloader_withhold_dns   = $apelpublisher::params::unloader_withhold_dns,
+  $ssm_enabled             = $apelpublisher::params::ssm_enabled,
+  $logging_logfile         = $apelpublisher::params::logging_logfile,
+  $logging_level           = $apelpublisher::params::logging_level,
+  $logging_console         = $apelpublisher::params::logging_console,) inherits apelpublisher::params {
+    
+  file { '/etc/apel/client.cfg':
+    owner   => "root",
+    group   => "root",
+    ensure  => "present",
+    content => template("${module_name}/client.cfg.erb"),
+    require => [Package['apel-client'],Package['apel-ssm']],
+  }
+  
+  file { '/etc/apel/sender.cfg':
+    owner   => "root",
+    group   => "root",
+    ensure  => "present",
+    content => template("${module_name}/sender.cfg.erb"),
+    require => Package['apel-ssm'],
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,33 @@
 class apelpublisher::params {
   $mysql_root_password       = "changeme"
   $mysql_apel_password       = "pleasechangeme"
-  $list_of_apel_parser_hosts = []
+  $list_of_apel_parser_hosts = [
+    ]
 
   $mysql_configure_backup    = true
   $mysql_backup_folder       = "/tmp/mysql_backup"
+
+  $mysql_hostname            = "localhost"
+  $mysql_port                = 3306
+  $mysql_database            = "apelclient"
+  $mysql_user                = "apel"
+
+  $site_name                 = "T4_LFT_Changeme"
+  $ldap_host                 = "lcg-bdii.cern.ch"
+  $ldap_port                 = 2170
+
+  $joiner_local_jobs         = false
+  $joiner_enabled            = true
+
+  $unloader_enabled          = true
+  $unloader_dir_location     = "/var/spool/apel/"
+  $unloader_send_summaries   = false
+  $unloader_withhold_dns     = false
+
+  $ssm_enabled               = true
+
+  $logging_logfile           = "/var/log/apel/client.log"
+  $logging_level                     = "INFO"
+  $logging_console                   = true
+
 }
