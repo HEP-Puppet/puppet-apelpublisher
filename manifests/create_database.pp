0a1504e1653598e5119db608f069ccf1100a1bcc
kreczko <lkreczko@gmail.com>
2013-06-27 12:53:46 +0100

    separating user creation from database creation

--- manifests/create_database.pp
+++ manifests/create_database.pp
@@ -6,17 +6,22 @@ class apelpublisher::create_database (
   $list_of_apel_parser_hosts = $apelpublisher::params::list_of_apel_parser_hosts,
   $configure_backup          = $apelpublisher::params::mysql_configure_backup,
   $mysql_backup_folder       = $apelpublisher::params::mysql_backup_folder,) inherits apelpublisher::params {
-  mysql::db { $mysql_database:
-    user     => $mysql_user,
-    password => $mysql_apel_password,
-    host     => 'localhost',
-    grant    => ['all'],
-  }
+  database_user { '${mysql_user}@localhost': password_hash => mysql_password($mysql_apel_password) }
 
-  create_resources('apelpublisher::db_permissions', $list_of_apel_parser_hosts)
+  mysql::db { $mysql_database: user => $mysql_user, }
+
+  $permission_defaults = {
+    'mysql_user'     => $mysql_user,
+    'mysql_database' => $mysql_database,
+  }
+  create_resources('apelpublisher::db_permissions', $list_of_apel_parser_hosts, $permission_defaults)
 
   exec { 'create-apel-mysql-tables':
     command => '/usr/bin/mysql -p apelclient < /usr/share/apel/client.sql',
-    require => [File['/usr/share/apel/client.sql'], Class["mysql"], Class["mysql::server"]],
+    require => [
+      File['/usr/share/apel/client.sql'],
+      Class["mysql"],
+      Class["mysql::server"],
+      Database[$mysql_database]],
   }
 }
--- manifests/db_permissions.pp
+++ manifests/db_permissions.pp
@@ -1,4 +1,7 @@
-define apelpublisher::db_permissions ($host = $title) {
-  database_grant { "apel@${host}/apelclient": privileges => [
+define apelpublisher::db_permissions (
+  $host           = $title,
+  $mysql_user     = $apelpublisher::params::mysql_user,
+  $mysql_database = $apelpublisher::params::mysql_database,) {
+  database_grant { "'${mysql_user}'@'${host}'/${mysql_database}": privileges => [
       'all'], }
 }
