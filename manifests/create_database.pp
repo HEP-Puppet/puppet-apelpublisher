000a221fa5d223ee4bf31bd250b22cd2b32c2847
kreczko <lkreczko@gmail.com>
2013-06-28 14:29:12 +0100

    adding backup setup

--- manifests/create_database.pp
+++ manifests/create_database.pp
@@ -3,9 +3,7 @@ class apelpublisher::create_database (
   $mysql_user                = $apelpublisher::params::mysql_user,
   $mysql_root_password       = $apelpublisher::params::mysql_root_password,
   $mysql_apel_password       = $apelpublisher::params::mysql_apel_password,
-  $list_of_apel_parser_hosts = $apelpublisher::params::list_of_apel_parser_hosts,
-  $configure_backup          = $apelpublisher::params::mysql_configure_backup,
-  $mysql_backup_folder       = $apelpublisher::params::mysql_backup_folder,) inherits apelpublisher::params {
+  $list_of_apel_parser_hosts = $apelpublisher::params::list_of_apel_parser_hosts,) inherits apelpublisher::params {
   mysql::db { $mysql_database:
     user     => $mysql_user,
     password => $mysql_apel_password
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,6 @@
 class apelpublisher::install (
   $mysql_root_password       = $apelpublisher::params::mysql_root_password,
+  $mysql_configure_backup    = $apelpublisher::params::mysql_configure_backup,
   $mysql_backup_folder       = $apelpublisher::params::mysql_backup_folder,
   $mysql_apel_password       = $apelpublisher::params::mysql_apel_password,
   $list_of_apel_parser_hosts = $apelpublisher::params::list_of_apel_parser_hosts) {
@@ -11,10 +12,14 @@ class apelpublisher::install (
   class { "apelpublisher::ca_policy_egi_core":
   }
 
-
-  package { ["apel-ssm", "apel-lib", "apel-client"]:
-    ensure   => present,
-    require  => [Yumrepo['epel'], Yumrepo['EMI_3_base']],
+  package { [
+    "apel-ssm",
+    "apel-lib",
+    "apel-client"]:
+    ensure  => present,
+    require => [
+      Yumrepo['epel'],
+      Yumrepo['EMI_3_base']],
   }
 
   Package["apel-ssm"] -> Package["apel-lib"] -> Package["apel-client"]
@@ -29,6 +34,14 @@ class apelpublisher::install (
   }
 
   class { 'mysql':
+  }
+
+  if $mysql_configure_backup {
+    class { 'mysql::backup':
+      backupuser     => 'root',
+      backuppassword => $mysql_root_password,
+      backupdir      => $mysql_backup_folder ,
+    }
   }
 
 }
