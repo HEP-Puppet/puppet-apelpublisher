0291babd2915ce5fc578aba9b0d31fd4899fa725
kreczko <lkreczko@gmail.com>
2013-07-03 16:17:23 +0100

    adding exception handling to apel_version fact (issue #1)

--- lib/facter/apel_version.rb
+++ lib/facter/apel_version.rb
@@ -6,6 +6,8 @@ Facter.add(:apel_version) do
   setcode do
     begin
       Facter::Util::Resolution.exec("/usr/bin/apelclient --version").split(" ")[2] 
+    rescue Exception
+          Facter.debug('apelclient not found')
     end
   end
 end
