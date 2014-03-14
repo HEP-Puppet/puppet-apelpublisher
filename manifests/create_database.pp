b4c83f04afa9ed2147f273364160db71e3cbe634
Robert Frank <robert.frank@manchester.ac.uk>
2014-03-14 14:03:45 +0000

    updated database creation check

--- manifests/create_database.pp
+++ manifests/create_database.pp
@@ -3,7 +3,9 @@ class apelpublisher::create_database (
   $mysql_user                = $apelpublisher::params::mysql_user,
   $mysql_root_password       = $apelpublisher::params::mysql_root_password,
   $mysql_apel_password       = $apelpublisher::params::mysql_apel_password,
-  $list_of_apel_parser_hosts = $apelpublisher::params::list_of_apel_parser_hosts,) inherits apelpublisher::params {
+  $list_of_apel_parser_hosts = $apelpublisher::params::list_of_apel_parser_hosts,
+) inherits apelpublisher::params {
+
   mysql::db { $mysql_database:
     user     => $mysql_user,
     password => $mysql_apel_password
@@ -22,10 +24,14 @@ class apelpublisher::create_database (
     command => "/usr/bin/mysql --defaults-file=/root/.my.cnf ${mysql_database} < /usr/share/apel/client.sql",
     require => [
       Class["apelpublisher::install"],
-      Mysql_database[$mysql_database]],
+      Mysql_database[$mysql_database]
+    ],
     # needs check if already exists, otherwise will wipe the tables!
     onlyif  => [
-      "/usr/bin/test `/usr/bin/mysql --defaults-file=/root/.my.cnf -e 'use ${mysql_database}; show tables;SELECT FOUND_ROWS();' 2>&1 | cut -f1 | egrep '^(0|[1-9][0-9]*)$'` -eq 0",
-      '/usr/bin/test -f /usr/share/apel/client.sql'],
+      # only run if the database exists and does not contain any tables (found rows == 0), don't run in all other cases
+      "/usr/bin/mysql --defaults-file=/root/.my.cnf -BNe 'show tables; SELECT FOUND_ROWS();' ${mysql_database} | /bin/grep '^0$'",
+      '/usr/bin/test -f /usr/share/apel/client.sql'
+    ],
+    logoutput => 'on_failure',
   }
 }
